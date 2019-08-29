50c3f4bba9991c4cc136c6f9f65f32966fa0491c
Samuel Dufour <samuel.dufour@arolla.fr>
2019-08-29 22:34:12 +0200

    Set skills storage connection string into app settings

--- SourceCode/Skills/SkillsFunc/AddEvaluationFunction.cs
+++ SourceCode/Skills/SkillsFunc/AddEvaluationFunction.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.IO;
@@ -12,16 +13,22 @@ namespace SkillsFunc
     public static class AddEvaluationFunction
     {
         [FunctionName("AddEvaluationFunction")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequest req, ILogger log)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequest req, ILogger log, ExecutionContext context)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
+            var config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+            var connectionString = config["SkillsStorageConnectionString"];
+
             string requestBody = new StreamReader(req.Body).ReadToEnd();
             var userSkill = JsonConvert.DeserializeObject<UserSkillDto>(requestBody);
-            var connectionString = req.Query["connectionString"];
 
             AddEvaluation(connectionString, userSkill);
-            
+
             return userSkill != null
                 ? (ActionResult)new OkObjectResult($"Hello, {userSkill}, {connectionString}")
                 : new BadRequestObjectResult($"Issue with the input {userSkill}");
