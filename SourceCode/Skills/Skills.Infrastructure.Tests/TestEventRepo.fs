178af8e56a57cfe90c839e3a40458705302b9be8
Samuel Dufour <samuel.dufour@arolla.fr>
2019-08-30 16:40:06 +0200

    Add event repository to save evaluation added event

---
+++ SourceCode/Skills/Skills.Infrastructure.Tests/TestEventRepo.fs
@@ -0,0 +1,21 @@
+﻿namespace Skills.Infrastructure.Tests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Skills.Infrastructure.EventStore
+open Skills.Infrastructure.EventRepo
+
+[<TestClass>]
+type TestEventRepo () =
+    //[<TestMethod>]
+    member this.``Given an evaluation added dto event When I save it Then it should persist``() =
+        let now = DateTime.Now
+        let eventToStore:EvaluationAddedDto = {
+            date = now
+            data = """{"evaluation":{"date":"2019-08-30T00:00:00","level":4,"skill":"poterie"},"user":{"name":"Machin"}}"""
+            eventType = "EvaluationAdded"
+        }
+
+        let connectionString = ""
+        saveEvent connectionString eventToStore |> ignore
+    
---
+++ SourceCode/Skills/Skills.Infrastructure/EventRepo.fs
@@ -0,0 +1,42 @@
+﻿namespace Skills.Infrastructure
+
+open System
+open Microsoft.WindowsAzure.Storage.Table
+open Microsoft.WindowsAzure.Storage
+open System.Threading.Tasks
+open EventStore
+open Newtonsoft.Json
+
+module EventRepo =
+
+    let partitionKey = "1"
+    let eventStoreTable = "eventstore"
+
+    type EventEntity (data : string) =
+        inherit TableEntity(partitionKey, Guid.NewGuid().ToString())
+        new() = EventEntity(null)
+        member val Data : string = data with get, set
+
+
+    let serialize (evaluationAddedDto:EvaluationAddedDto) =
+        evaluationAddedDto |> JsonConvert.SerializeObject
+    
+
+    let getEventStoreTable connectionString =
+        let storageAccount = CloudStorageAccount.Parse(connectionString)
+        let tableClient = storageAccount.CreateCloudTableClient()
+        let table = tableClient.GetTableReference(eventStoreTable)
+        table
+
+    let saveEvent connectionString evaluationAddedDto =
+        let table = connectionString |> getEventStoreTable
+        let jsonEvent = 
+            evaluationAddedDto
+            |> serialize
+        let insertOperation = TableOperation.InsertOrReplace(new EventEntity(jsonEvent))
+        async {
+            let! result = Async.AwaitTask (table.ExecuteAsync(insertOperation))
+            return result.Result
+        } |> Async.RunSynchronously |> ignore
+
+
