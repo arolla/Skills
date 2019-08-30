c82362b7a4bf48829eb976887b0410c5fc587ba8
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2019-08-30 17:16:36 +0200

    Add missing open

--- SourceCode/Skills/Skills.Infrastructure.Tests/TestEventStore.fs
+++ SourceCode/Skills/Skills.Infrastructure.Tests/TestEventStore.fs
@@ -4,6 +4,7 @@ open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
 open Skills.Domain.Event
 open Skills.Infrastructure.EventStore
+open Skills.Domain.UserSkillEvaluation
 
 [<TestClass>]
 type TestEventStore () =
